da160bc1877583f15f07b2269964c495eb676745
Igor Oliveira <ioliveir@thoughtworks.com>
2017-03-01 19:44:03 -0300

    Add new tests

--- app/src/test/java/org/pestrada/android_tdd_playground/ExampleUnitTest.java
+++ app/src/test/java/org/pestrada/android_tdd_playground/ExampleUnitTest.java
@@ -8,8 +8,15 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void additionShouldBeCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void divisionShouldBeCorrect() {
+        assertEquals(2, 8 / 4);
+    }
+
 }
